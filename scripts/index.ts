e7da89ace3be5cbd49a48ec0ea9823a92c234265
Harpica <harpy.ka@gmail.com>
2022-11-28 00:46:21 +0300

    style: buttons + feat: open-popup with settings

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,20 +1,32 @@
 import { Grid, ActionType } from './modules/draw.js';
+import { openPopup, closePopup, togglePopup } from './modules/utils.js';
 
-const gridSizeElement =
-  document.querySelector('.content__range') as HTMLInputElement;
+const gridSizeElement = document.querySelector(
+  '.content__range'
+) as HTMLInputElement;
 const gridElement = document.querySelector('.content__grid') as HTMLElement;
 
 const grid = new Grid(parseInt(gridSizeElement.value), gridElement);
 
 const gridSizeForm = document.querySelector('.content__form') as HTMLElement;
 
-const gridSizeCounter = document.querySelector('.content__label_type_value') as HTMLElement;
+const gridSizeCounter = document.querySelector(
+  '.content__label_type_value'
+) as HTMLElement;
 
 const rainbowButton = document.querySelector('.button__rainbow') as HTMLElement;
 
 const clearButton = document.querySelector('.button__clear') as HTMLElement;
 
-const colorInput = document.querySelector('.content__color') as HTMLInputElement;
+const colorInput = document.querySelector(
+  '.content__color'
+) as HTMLInputElement;
+
+const popup = document.querySelector('.popup') as HTMLElement;
+
+const openPopupButton = document.querySelector(
+  '.button__open-popup'
+) as HTMLElement;
 
 // Changing size of grid
 gridSizeForm.addEventListener('submit', (evt: Event) => {
@@ -41,3 +53,10 @@ clearButton.addEventListener('click', () => {
 colorInput.addEventListener('input', () => {
   grid.changeColor(colorInput.value);
 });
+
+// Opening popup
+openPopupButton.addEventListener('click', () => {
+  togglePopup(popup);
+});
+
+
